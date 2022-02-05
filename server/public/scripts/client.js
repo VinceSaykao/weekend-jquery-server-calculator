5615d3e573d8f59b958f98cb9b37c187214fefa9
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-02-04 21:31:30 -0600

    got add button to work with equal button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,40 @@ $(document).ready(onReady);
 
 function onReady() {
     $('#equal').on('click', equalButtonDo);
-    $('#c').on('click', clearButtonDo)
+    $('#c').on('click', clearButtonDo);
+    $('#plus').on('click', addInputs)
 }; // end of function
 
-
+// when equal button is pressed, will calculate both inputs
 function equalButtonDo() {
+    
     $('#result').append('<div>',
-    ($('#first-input').val())
+    sum
     ,'</div>');
+    addInputs();
+    
 }; // end of function 
 
+ // ($('#first-input').val())
+
+// this will clear the results and input fields
 function clearButtonDo() {
     $('#result').empty();
+    ($('#first-input').val(''));
+    ($('#second-input').val(''));
+}; // end of function
 
+// this will add both inputs together
+function addInputs() {
+    a = Number(($('#first-input').val()));
+    b = Number(($('#second-input').val()))
+    sum = (a + b);
+    console.log(sum);
+    return sum;
+    
 
 }; // end of function
+
+
+
 
